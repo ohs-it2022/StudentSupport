6e69156ffef59535f210d44b08ad6b3fe88f11c4
Toshiki <wata.hal.student@gmail.com>
2023-05-15 12:03:20 +0900

    add_detail追加

---
+++ lib/src/screens/register/add_detail.dart
@@ -0,0 +1,18 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:student_support/src/sample.dart';
+
+@RoutePage()
+class AddDetail extends StatelessWidget {
+  const AddDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenWidget(
+      titleTxt: '単元詳細', 
+      bodyContents: Center(
+        child: Text('AAA'),
+      )
+    );
+  }
+}
