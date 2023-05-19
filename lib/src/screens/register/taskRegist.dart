aeddccacce351b6e5a23dc5c409133c97598c9b7
Toshiki <wata.hal.student@gmail.com>
2023-05-19 10:25:02 +0900

    課題登録画面追加

--- lib/src/screens/register/taskRegist.dart
+++ lib/src/screens/register/taskRegist.dart
@@ -3,10 +3,51 @@ import 'package:flutter/material.dart';
 import 'package:student_support/src/bottom_bar.dart';
 import 'package:student_support/src/sample.dart';
 
-const _bodyContents = Center(
+final _work = Container(
+  width: double.infinity,
+  margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+  decoration: BoxDecoration(
+    color: bgColor2,
+    borderRadius: BorderRadius.circular(30),
+  ),
+  child: Container(
+    margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+    padding: EdgeInsets.all(20),
+    decoration: BoxDecoration(
+      color: bgColor1,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'タイトル',
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: '教科',
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: '提出期限',
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: '内容',
+          ),
+        ),
+      ],
+    ),
+  ),
+);
+
+final _bodyContents = Center(
   child: Column(
     children: [
-      Expanded(child: BasicText(text: '課題登録', size: 20),),
+      Expanded(child: _work),
       BottomBar(selected: 3,)
     ],
   )
@@ -19,7 +60,7 @@ class TaskRegistPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ScreenWidget(
+    return ScreenWidget(
       titleTxt: '課題登録', 
       bodyContents: _bodyContents,
       drawerFlg: 1,
