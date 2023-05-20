7c7f8606f8130fa1f8ab49cf5ff1c90de28afa57
Toshiki <wata.hal.student@gmail.com>
2023-05-20 22:06:23 +0900

    月→日の遷移追加

--- lib/src/screens/home/home_month_page.dart
+++ lib/src/screens/home/home_month_page.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:student_support/routers/app_router.gr.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 DateTime _focusedDay = DateTime.now();
 DateTime? _selected;
+CalendarFormat _calendarFormat = CalendarFormat.month;
 
 @RoutePage()
 class HomeMonthPage extends StatefulWidget {
@@ -21,16 +23,25 @@ class _HomeMonthPageState extends State<HomeMonthPage> {
         firstDay: DateTime.utc(2023, 4, 1),
         lastDay: DateTime.utc(2024, 3, 31),
         selectedDayPredicate: (day) {
-              return isSameDay(_selected, day);
-            },
-            onDaySelected: (selected, focused) {
-              if (!isSameDay(_selected, selected)) {
-                setState(() {
-                  _selected = selected;
-                  _focusedDay = focused;
-                });
-              }
-            },
+          return isSameDay(_selected, day);
+        },
+        calendarFormat: _calendarFormat,
+        onDaySelected: (selected, focused) {
+          // if (!isSameDay(_selected, selected)) {
+          //   setState(() {
+          //     _selected = selected;
+          //     _focusedDay = focused;
+          //   });
+          // }
+          AutoRouter.of(context).pushNamed('/home/day');// 変更が必要
+        },
+        onFormatChanged: (format){
+          if (_calendarFormat != format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
         focusedDay: _focusedDay,
       ),
     );
