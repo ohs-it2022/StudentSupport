f9aa17bc3ce47dcf8b96517dc9986dff00a552f9
Toshiki <wata.hal.student@gmail.com>
2023-06-10 19:45:45 +0900

    フォーム画面追記

--- lib/src/screens/timeTable/form.dart
+++ lib/src/screens/timeTable/form.dart
@@ -16,26 +16,23 @@ class FormPage extends StatelessWidget {
     List<List<String>> weekTimeTableAdd = weekTimeTable as List<List<String>>;
 
     return ScreenWidget(
-      titleTxt: '単元追加', 
-      bodyContents: Center(
-        child: Column(
+      titleTxt: '時間割追加', 
+      bodyContents: Column(
           children: [
-            TextField(
-              decoration: InputDecoration(hintText: '表示名'),
-              onChanged: (value) => (titleSubName = value),
+            Align(
+              alignment: Alignment.centerRight,
+              child: ElevatedButton(
+                onPressed: (){}, 
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red
+                ),
+                child: Text("削除")
+              ),
             ),
-            TextField(
-              decoration: InputDecoration(hintText: '正式名称(任意)'),
-              onChanged: (value) => (subName = value),
-            ),
-            TextField(
-              decoration: InputDecoration(hintText: '教室名(任意)'),
-              onChanged: (value) => (classRoom = value),
-            ),
-            TextField(
-              decoration: InputDecoration(hintText: '教科担当(任意)'),
-              onChanged: (value) => (teacher = value),
-            ),
+            formElem(txt: "表示名", initTxt: "CS11", function: (value) => (titleSubName = value)),
+            formElem(txt: "正式名称", initTxt: "コンピュータサイエンス", function: (value) => (subName = value)),
+            formElem(txt: "教室名", initTxt: "161", function: (value) => (classRoom = value),),
+            formElem(txt: "教科担当", initTxt: "渡瀬健", function: (value) => (teacher = value),),
             ElevatedButton(
               onPressed: (){
 
@@ -44,6 +41,46 @@ class FormPage extends StatelessWidget {
             )
           ],
         ),
+    );
+  }
+}
+
+
+class formElem extends StatelessWidget {
+  final String txt;
+  final String initTxt;
+  final function;
+  const formElem({super.key, required this.txt, this.initTxt="", required this.function});
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;  // 画面のサイズを取得
+
+    return Center(
+      child: SizedBox(
+        width: screenSize.width * 0.95,
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: screenSize.width * 0.2,
+                child: Center(child: Text(txt)),
+              ),
+              SizedBox(
+                width: screenSize.width * 0.7,
+                child: Flexible(
+                  child: TextField(
+                    controller: TextEditingController(text: initTxt),
+                    onChanged: function,
+                  )
+                ),
+              )
+            ],
+          ),
+        )
+        
       )
     );
   }
